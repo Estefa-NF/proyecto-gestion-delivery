81fdce8c6ef13847570ae33ef3bcde19e0a80295
estefano <eaaronmarmanir1@gmail.com>
2026-08-28 11:19:36 -0500

    avance fusion

---
+++ src/ConexionDB.java
@@ -0,0 +1,23 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+    public class ConexionDB {
+        private static final String URL = "CONEXION";
+
+        public static Connection conectar() {
+            Connection conexion = null;
+            try {
+                conexion = DriverManager.getConnection(URL);
+                System.out.println("Conectado a la base de datos del delivery.");
+            } catch (SQLException e) {
+                System.out.println("Error de conexión" + e.getMessage());
+            }
+            return conexion;
+        }
+
+        public static void main(String[] args) {
+            conectar();
+        }
+    }
+
